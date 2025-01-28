626994085c0a8f13fb5b1ef4c3f50f880ba94011
Anjalinegi <anjalinegi26500@gmail.com>
2025-01-29 02:48:05 +0530

    Update ageCalc.java

--- ageCalc.java
+++ ageCalc.java
@@ -2,7 +2,7 @@ import java.time.LocalDate;
 import java.time.Period;
 import java.util.Scanner;
 
-public class AgeCalculator {
+public class ageCalc {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
